3974ac082f31c97cefa33998ad97892d1bbd4e87
stas <gorelkov@campus.tu-berlin.de>
2022-12-23 13:46:14 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,23 @@
+import sys
+import requests
+import bs4 as BeautifulSoup
+
+
+# print()
+# ## shows the version of python we are using
+# print(f"You are using Python version: {sys.version}")
+# print()
+# ## prints out the location where python is located on this mashine
+# print(f"Your python interpreter is installed here: {sys.executable}")
+
+
+# def greet(who_to_greet):
+#     greeting = "Hello, {}!".format(who_to_greet)
+#     return greeting
+
+
+# print(greet("Stas"))
+# r = requests.get("https://coreyms.com")
+# print(r.status_code)
+name = input('Your name?')
+print('Hello,', name)
